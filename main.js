51a8bcf1d799c85b1f5f5890ff5153ceb05306ec
thomaspaysac <113386819+thomaspaysac@users.noreply.github.com>
2025-04-15 10:37:35 +0200

    Add homepage translation

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-import { translateHeader } from "./traduction/header";
-
 // Get current language from session storage, set to FR if null
 (function setDefaultLang () {
   if (!sessionStorage.getItem("lang")) {
@@ -26,9 +24,43 @@ EN_BUTTON.addEventListener("click", () => {
   }
 });
 
+
 // If language is set to french, display HTML. Else, translate DOM
 (function translateDOM () {
   if (sessionStorage.getItem("lang") !== "fr") {
     translateHeader();
+    translateHomepage();
   }
 })()
+
+
+
+
+
+
+
+// TRANSLATION FUNCTIONS
+function translateHeader () {
+  const HOME_NAV_BUTTON = document.getElementById("home__nav-button");
+  HOME_NAV_BUTTON.textContent = "Home";
+  
+  const GROUPS_NAV_BUTTON = document.getElementById("groups__nav-button");
+  GROUPS_NAV_BUTTON.textContent = "Groups";
+  
+  const TEAM_NAV_BUTTON = document.getElementById("team__nav-button");
+  TEAM_NAV_BUTTON.textContent = "Team";
+  
+  const PARENTS_NAV_BUTTON = document.getElementById("parents__nav-button");
+  PARENTS_NAV_BUTTON.textContent = "Parents"; 
+}
+
+function translateHomepage () {
+  const HOME_WELCOME = document.getElementById("home_welcome");
+  HOME_WELCOME.innerHTML = "Welcome to <br/>Crèche des Morillons";
+
+  const HOME_HEADER_2 = document.getElementById("home_header2");
+  HOME_HEADER_2.innerHTML = "An institution subsidized by <br/>the City of Geneva";
+
+  const HOME_MESSAGE = document.getElementById("home_parents-message");
+  HOME_MESSAGE.innerHTML = "To register youor child, please contact the <a href='https://www.geneve.ch/demarches/demande-place-creche'>Early Childhood Information Office (BIPE) of the City of Geneva</a>";
+}
