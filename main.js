cec0be64837373bad880be19487a9d556e3087f5
thomaspaysac <113386819+thomaspaysac@users.noreply.github.com>
2025-09-09 10:29:28 +0200

    Animate mobile navbar icon when opening/closing the menu

--- main.js
+++ main.js
@@ -1,25 +1,19 @@
 // Dropdown
-let dropdown_open = false;
+let sidebar_open = false;
 
 const OPEN_NAV_BUTTON = document.getElementById("mobile-nav__open");
 const MOBILE_SIDEBAR =  document.getElementsByClassName("mobile-sidebar")[0];
+const MOBILE_NAV_ICON = document.getElementById("mobile-nav__icon");
+
 OPEN_NAV_BUTTON.addEventListener("click", () => {
-  dropdown_open = !dropdown_open;
-  animateSidebar(dropdown_open);
+  MOBILE_SIDEBAR.classList.toggle("open-sidebar");
+  sidebar_open = !sidebar_open;
+  changeSidebarIcon(sidebar_open);
 });
 
-function animateSidebar (status) {
-  /*if (status) {
-    MOBILE_SIDEBAR.classList.add("open-sidebar");
-  } else {
-    console.log('false')
-  }*/
- MOBILE_SIDEBAR.classList.toggle("open-sidebar");
+function changeSidebarIcon (sidebarStatus) {
+  sidebarStatus ? MOBILE_NAV_ICON.src = "./img/icons/close_menu.png" : MOBILE_NAV_ICON.src = "./img/icons/menu.png";
 }
-
-
-
-
 
 
 // Get current language from session storage, set to FR if null
